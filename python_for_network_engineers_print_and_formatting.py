babca54030bf299be8e31192be3c48da1042d46b
mmorris0509 <crossover2388@hotmail.com>
2025-04-26 07:48:42 -0400

    changing file to reflect 15 devices again

--- python_for_network_engineers_print_and_formatting.py
+++ python_for_network_engineers_print_and_formatting.py
@@ -1,7 +1,7 @@
 #Testing variables in Python
 Router_Type = " ASR9K"
 SW_Version = "6.4.2"
-Number_Of_Nodes = 14
+Number_Of_Nodes = 15
 #print('Our network is made of ' + str(Number_Of_Nodes) + Router_Type + " running S/W image " + SW_Version)
 My_Network = f"Our network is made of {Number_Of_Nodes} {Router_Type} running S/W image {SW_Version}"
 print(My_Network)
